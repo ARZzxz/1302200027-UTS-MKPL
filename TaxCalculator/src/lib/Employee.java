259e33283307afe41c680b218d3020a925349243
Achmad Rizky <97281297+ARZzxz@users.noreply.github.com>
2023-04-14 20:48:06 +0700

    Membuat class baru yaitu child & Spouse

--- TaxCalculator/src/lib/Employee.java
+++ TaxCalculator/src/lib/Employee.java
@@ -61,39 +61,3 @@ public class Employee {
 	}
 }
 }
-
-public class Spouse {
-    private String name;
-    private String idNumber;
-    
-    public Spouse(String name, String idNumber) {
-        this.name = name;
-        this.idNumber = idNumber;
-    }
-    
-    public String getName() {
-        return name;
-    }
-    
-    public String getIdNumber() {
-        return idNumber;
-    }
-}
-
-public class Child {
-    private String name;
-    private String idNumber;
-    
-    public Child(String name, String idNumber) {
-        this.name = name;
-        this.idNumber = idNumber;
-    }
-    
-    public String getName() {
-        return name;
-    }
-    
-    public String getIdNumber() {
-        return idNumber;
-    }
-}
